f64a80d2c65413614e4a4bdcfe95d95aea130c48
BigTatyanos <66073160+BigTatyanos@users.noreply.github.com>
2021-02-28 23:21:49 +0500

    add getHeroFromAnswer

--- src/appBot/Question.java
+++ src/appBot/Question.java
@@ -4,4 +4,8 @@ import java.util.Map;
 public class Question {
     private String question;
     private Map<String, Hero> answers;
+
+    public Hero getHeroFromAnswer(String answer){
+        return this.answers.get(answer);
+    }
 }
