331237ad799b7e65d90dea7f8c94dc5cf38fa189
BigTatyanos <66073160+BigTatyanos@users.noreply.github.com>
2021-02-28 23:21:06 +0500

    add enterProgress, getQuestion, getResult, dropRes

--- src/appBot/Test.java
+++ src/appBot/Test.java
@@ -3,6 +3,47 @@ import java.util.*;
 
 public class Test {
     private String name;
-    private Set<Question> questions;
+    public Set<Question> questions;
+    public Set<Question> usedQuestions;
     private Map<Hero, Integer> progress;
+
+    public Test() {
+    }
+
+    private void enterProgress(Hero hero){
+        if(this.progress.containsKey(hero))
+            this.progress.put(hero, progress.get(hero) + 1);
+        else
+            this.progress.put(hero, 1);
+    }
+
+    private Question getQuestion(){
+        for (Question question : questions) {
+            this.usedQuestions.add(question);
+            this.questions.remove(question);
+            return question;
+        }
+        return null;
+    }
+
+    public Hero getResult(){
+        Integer maxRes = Collections.max(this.progress.values());
+
+        for (Hero hero : progress.keySet()) {
+            Integer obj = progress.get(hero);
+            if (hero != null) {
+                if (maxRes.equals(obj)) {
+                    return hero;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void dropResultsTest(){
+        this.questions.addAll(this.usedQuestions);
+        this.usedQuestions.clear();
+        this.progress.clear();
+    }
+
 }
