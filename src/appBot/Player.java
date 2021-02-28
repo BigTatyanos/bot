686ff5f686d50e3291fc795f5ea6e21d897ac97b
BigTatyanos <66073160+BigTatyanos@users.noreply.github.com>
2021-02-28 23:19:25 +0500

    add getHeroes

--- src/appBot/Player.java
+++ src/appBot/Player.java
@@ -3,5 +3,9 @@ import java.util.*;
 
 public class Player {
     private String name;
-    private Map<Test, Hero> heroes;
+    public Map<Test, Hero> heroes;
+
+    public Set<Hero> getHeroes(){
+        return new HashSet<Hero>(heroes.values());
+    }
 }
