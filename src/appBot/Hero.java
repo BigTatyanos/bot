1ad1edb2c872a030d0cf6bd803dba48e71b22913
Dambldore <valrusal@outlook.com>
2021-03-05 14:05:22 +0500

    Update Hero.java

--- src/appBot/Hero.java
+++ src/appBot/Hero.java
@@ -3,5 +3,36 @@ package appBot;
 public class Hero {
     private String testName;
     private String name;
-    private String discription;
+    private String description;
+    
+    public Hero(String testName, String name, String description) {
+        this.testName = testName;
+        this.name = name;
+        this.description = description;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getTestName(){
+        return this.testName;
+    }
+
+    public void setTestName(String testName){
+        this.testName = testName;
+    }
+
+    public String getDiscription(){
+        return this.description;
+    }
+
+    public void setDiscription(String discription){
+        this.description = discription;
+    }
 }
