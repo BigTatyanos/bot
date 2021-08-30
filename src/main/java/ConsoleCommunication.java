2322f41fff15ac4baa96a54416d64576a3049fa6
Dambldore <valrusal@outlook.com>
2021-08-30 17:57:48 +0500

    Delete method HandleUserCommand

--- src/main/java/ConsoleCommunication.java
+++ src/main/java/ConsoleCommunication.java
@@ -1,24 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class ConsoleCommunication {
     private Scanner in = new Scanner(System.in);
-    public String getUserInput(){
+
+    public static void printText(String text) {
+        System.out.println(text);
+    }
+
+    public String getUserInput() {
         String input = in.nextLine();
         return input;
     }
 
-    public void closeScanner(){
+    public void closeScanner() {
         in.close();
-    }
-
-    public static void printText(String text) {
-        System.out.println(text);
     }
 
     public void printHello() {
         String message = "Привет, это ТестБот.";
         printText(message);
     }
+
     public String getPlayerName() {
         printText("Как Вас зовут?");
         String playerName = getUserInput();
@@ -32,43 +34,5 @@ public class ConsoleCommunication {
         printText("Введи /exit, чтобы закончить игру");
         printText("Введи /heroes, чтобы вывести список своих полученных персонажей");
         printText("Это тест бот, в котором нужно выбирать тест и отвечать на вопросы, а в конце ты узнаешь, кто ты из персонажей");
-    }
-
-    public boolean HandleUserCommand(Game game){
-        String userInput = getUserInput();
-        if(userInput.equals("/help")){
-            getHelp();
-            return false;
-        }
-        else if (userInput.equals("/start")) {
-            printText("Какой тест хочешь пройти? Вот список тестов:");
-            for(String testName: game.getTestsNames())
-                printText(testName);
-            return false;
-        }
-        else if (userInput.equals("/exit")) {
-            printText("Пока, до скорой встречи!");
-            return true;
-        }
-        else if (userInput.equals("/heroes")) {
-            Set<Hero> heroes = game.getPlayer().getHeroes();
-            if(heroes.isEmpty()){
-                printText("Ты ещё не прошёл ни одного теста");
-            }
-            else {
-                for(Hero hero: heroes){
-                    printText(hero.getName());
-                }
-            }
-            return false;
-        }
-        else if(game.getTestsNames().contains(userInput)) {
-            game.setCurrentTest(game.findTest(userInput));
-            return false;
-        }
-        else{
-            printText("Некорректная строка");
-            return false;
-        }
     }
 }
