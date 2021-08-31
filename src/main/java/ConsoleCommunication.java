c68ce603ec5fd3e6f1c515f36f5cc312e0da9c4f
Dambldore <valrusal@outlook.com>
2021-08-31 10:54:19 +0500

    Remove public access

--- src/main/java/ConsoleCommunication.java
+++ src/main/java/ConsoleCommunication.java
@@ -1,27 +1,27 @@
 import java.util.List;
 import java.util.Scanner;
 
-public class ConsoleCommunication {
+class ConsoleCommunication {
     private Scanner in = new Scanner(System.in);
 
-    public static void printText(String text) {
+    static void printText(String text) {
         System.out.println(text);
     }
 
-    public String getUserInput() {
+    String getUserInput() {
         return in.nextLine();
     }
 
-    public void closeScanner() {
+    void closeScanner() {
         in.close();
     }
 
-    public void printHello() {
+    void printHello() {
         String message = "Привет, это ТестБот.";
         printText(message);
     }
 
-    public String getPlayerName() {
+    String getPlayerName() {
         printText("Как Вас зовут?");
         String playerName = getUserInput();
         getHelp();
