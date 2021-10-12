b760f4fd53c98d4129250fa23e344eea12b8cbc3
Dambldore <valrusal@outlook.com>
2021-10-12 21:41:10 +0500

    Refactor classes

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -5,20 +5,24 @@ public class Question {
     private String question;
     private Map<String, Hero> answers;
 
-    public Hero getHeroFromAnswer(String answer){
-        return this.answers.get(answer);
-    }
-
-    public Question(String question, Map<String, Hero> answers){
+    public Question(String question, Map<String, Hero> answers) {
         this.question = question;
         this.answers = answers;
     }
 
-    public boolean checkValidAnswer(String answer){
+    public Hero getHeroFromAnswer(String answer) {
+        return this.answers.get(answer);
+    }
+
+    public boolean checkValidAnswer(String answer) {
         return answers.containsKey(answer);
     }
 
-    public String getQuestion() { return this.question; }
+    public String getQuestion() {
+        return this.question;
+    }
 
-    public Set<String> getAnswers() { return this.answers.keySet(); }
+    public Set<String> getAnswers() {
+        return this.answers.keySet();
+    }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,3 @@
-//package appBot;
 import java.util.*;
 
 public class Test {
@@ -8,34 +7,32 @@ public class Test {
     private Map<Hero, Integer> progress;
     private Question currentQuestion;
 
-    public Test(String name, List<Question> questions){
+    public Test(String name, List<Question> questions) {
         this.name = name;
         this.questions = questions;
         this.progress = new HashMap<>();
         this.usedQuestions = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
+    public List<Question> getQuestions() {
+        return questions;
     }
 
-    public List<Question> getQuestions() {return questions; }
-
-    public void enterProgress(Hero hero){
-        if(progress.containsKey(hero))
+    public void enterProgress(Hero hero) {
+        if (progress.containsKey(hero))
             progress.put(hero, progress.get(hero) + 1);
         else
             progress.put(hero, 1);
     }
 
-    public Question getQuestion(){
+    public Question getQuestion() {
         Question question = questions.get(0);
         usedQuestions.add(question);
         questions.remove(question);
         return question;
     }
 
-    public Hero getResult(){
+    public Hero getResult() {
         Integer maxRes = Collections.max(progress.values());
 
         for (Hero hero : progress.keySet()) {
@@ -49,7 +46,7 @@ public class Test {
         return null;
     }
 
-    public void dropResultsTest(){
+    public void dropResultsTest() {
         this.questions.addAll(usedQuestions);
         this.usedQuestions.clear();
         this.progress.clear();
